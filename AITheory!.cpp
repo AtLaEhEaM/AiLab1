5086334cdac9e59a1dac7f44498a06eabf33d21a
AtLaEhEaM <tahaaleem885@gmail.com>
2025-10-02 21:53:05 +0500

    Display Graph
    graph is now displayed

--- AITheory!.cpp
+++ AITheory!.cpp
@@ -1,21 +1,25 @@
 #include<iostream>
 #include<vector>
+#include<utility>
 using namespace std;
 
 
-struct Node { //NoDe
-	int nodeNumber = 0; //assign node number to keep track of shit
-	vector<Edge> edges; //since all edges are outgoing we do not need seperation for in and out
+struct Edge {
+    int connectedNode;
+    double price;
 };
 
-struct Edge {
-	int connectedNode;
-    int price;
+struct Node { //NoDe
+    int nodeNumber = 0; //assign node number to keep track of shit
+    vector<Edge> edges; //since all edges are outgoing we do not need seperation for in and out
 };
 
 struct GraphData { //since values are provided beforehand we can hard code them >:D
     vector<Node> nodes;
 };
+
+// forward declaration
+void DisplayGraph(const GraphData& g);
 
 GraphData InitializeNodes() {
     vector<vector<pair<int, double>>> graph = {
@@ -53,11 +57,10 @@ int main() {
     GraphData graphData = InitializeNodes();
 
     //testing
-    for (int i = 0; i < graphData.nodes.size(); i++) {
-
-    }
+    DisplayGraph(graphData);
+    return 0;
 }
-
+s
 void DisplayGraph(const GraphData& g) {
     for (int i = 1; i < g.nodes.size(); i++) {   // start at 1 (node 0 unused)
         cout << "Node " << g.nodes[i].nodeNumber << " -> ";
@@ -66,14 +69,14 @@ void DisplayGraph(const GraphData& g) {
             cout << "(no connections)";
         }
         else {
-            
+
             // c++11 feature 
             //auto self defines type
             //ampersand is used to reference the node instead of copying it 
             //const makes sure we do not update any value 
-            */
-            for (const auto& e : g.nodes[i].edges) { 
-                cout << "Node " << e.to << " (p=" << e.prob << ") ";
+
+            for (const auto& e : g.nodes[i].edges) {
+                cout << "Node " << e.connectedNode << " (p=" << e.price << ") ";
             }
         }
         cout << "\n";
---
+++ Test2/Test2/Main.cpp
@@ -0,0 +1,84 @@
+#include<iostream>
+#include<vector>
+#include<utility>
+using namespace std;
+
+
+struct Edge {
+    int connectedNode;
+    double price;
+};
+
+struct Node { //NoDe
+    int nodeNumber = 0; //assign node number to keep track of shit
+    vector<Edge> edges; //since all edges are outgoing we do not need seperation for in and out
+};
+
+struct GraphData { //since values are provided beforehand we can hard code them >:D
+    vector<Node> nodes;
+};
+
+// forward declaration
+void DisplayGraph(const GraphData& g);
+
+GraphData InitializeNodes() {
+    vector<vector<pair<int, double>>> graph = {
+        {}, // node 0 unused
+        { {2, 0.2}, {3, 0.9}, {9, 0.9} },
+        { {7, 0.8}, {3, 0.1} },
+        { {5, 0.1}, {4, 0.1}, {7, 0.35} },
+        { {8, 0.25} },
+        { {4, 0.1} },
+        { {10, 0.5} },
+        { {6, 0.35}, {5, 0.1} },
+        { {10, 0.4} },
+        { {4, 0.1} },
+        {}
+    };
+
+    GraphData graphData;
+    graphData.nodes.resize(graph.size()); // allocate all nodes
+
+    for (int i = 1; i < graph.size(); i++) {
+        graphData.nodes[i].nodeNumber = i;
+
+        for (int j = 0; j < graph[i].size(); j++) {
+            Edge e;
+            e.connectedNode = graph[i][j].first;   // destination node
+            e.price = graph[i][j].second;  // probability
+            graphData.nodes[i].edges.push_back(e);
+        }
+    }
+
+    return graphData;
+}
+
+int main() {
+    GraphData graphData = InitializeNodes();
+
+    //testing
+    DisplayGraph(graphData);
+    return 0;
+}
+
+void DisplayGraph(const GraphData& g) {
+    for (int i = 1; i < g.nodes.size(); i++) {   // start at 1 (node 0 unused)
+        cout << "Node " << g.nodes[i].nodeNumber << " -> ";
+
+        if (g.nodes[i].edges.empty()) {
+            cout << "(no connections)";
+        }
+        else {
+
+            // c++11 feature 
+            //auto self defines type
+            //ampersand is used to reference the node instead of copying it 
+            //const makes sure we do not update any value 
+
+            for (const auto& e : g.nodes[i].edges) {
+                cout << "Node " << e.connectedNode << " (p=" << e.price << ") ";
+            }
+        }
+        cout << "\n";
+    }
+}
